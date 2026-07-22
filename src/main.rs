eb90d8740f34c1ad47f5b0a2e7a94dffc13df2c6
Yugaaank <yugaankrathore0@gmail.com>
2026-07-22 23:12:40 +0530

    chore: project scaffold

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,4 @@
-mod config;
-
 fn main() {
     env_logger::init();
-    let cfg = config::Config::load();
-    log::info!("floating-hide starting (debug={})", cfg.debug);
+    log::info!("floating-hide starting");
 }
